911017e3c7065d050174af1f816d7f3635f54464
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-03 06:27:20 +0800

    Attempt to improve the code again
    Attempted to improve by running two separate pipelines sequentially.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Box, Card, Heading, SimpleGrid } from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
-import { parse2, parse3 } from "./utils/parser";
+import { parse3 } from "./utils/parser";
 
+import { Root as HastRoot } from "hast";
+import { Root as MdastRoot } from "mdast";
 import rehypeReact from "rehype-react";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
+import remarkStringify from "remark-stringify";
 import { unified } from "unified";
+import { visit } from "unist-util-visit";
 import Bold from "./components/text/Bold";
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
@@ -19,21 +23,34 @@ const App: React.FC = () => {
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    parse(text).then((parsedText) => {
-      setParsed(parsedText.result);
-    });
-    parse2(text).then((parsedText2) => {
-      setContent(String(parsedText2));
+    parse2(text).then((parsedText) => {
+      // setParsed(String(parsedText));
+      parse(String(parsedText)).then((parsedText) => {
+        setParsed(parsedText.result);
+      });
     });
   }, [text]);
 
   const ref = useRef<HTMLDivElement>(null);
-  console.log(ref.current?.innerHTML);
+  // console.log(ref.current?.innerHTML);
+
+  let i = 0;
+  const nodes: any[] = [];
+  const nodes2: any[] = [];
+  const logger = () => (tree: HastRoot) => {
+    visit(tree, (node: any) => {
+      if (node.type === "text") {
+        node.id = i++;
+        // console.log(node);
+        nodes.push(node);
+      }
+    });
+  };
 
   const parser = unified()
     .use(remarkParse)
     .use(remarkRehype)
-    // .use(logger)
+    .use(logger)
     .use(rehypeReact, {
       createElement: React.createElement,
       Fragment: React.Fragment,
@@ -43,21 +60,36 @@ const App: React.FC = () => {
           React.createElement(
             Bold,
             {
-              callback: async (e: HTMLElement) => {
+              blurCallback: (e: EventTarget & Element) => {
+                console.log(e.innerHTML);
+              },
+              callback: async (node) => {
                 // console.log(ref.current?.innerHTML);
-                // console.log(e);
+                // console.log(tokenRef.current?.innerText);
+                // const offset = node.position.start.offset;
+                const id = node.id;
+                // console.log(id);
+                console.log(nodes.length);
+                console.log(nodes2.length);
+                console.log(nodes);
+                console.log(nodes2);
 
-                const val = e.innerHTML;
-                console.log(val);
-                e.blur();
-                await sleep(0);
-                e.innerHTML = val;
-                console.log(ref.current?.innerHTML);
+                // const val = e.innerHTML;
+                // console.log(val);
+                // e.outerHTML = val;
+                // e.blur();
+                // await sleep(0);
+                // // parse2(`**${val}**`).then((text) => {
+                // //   e.outerHTML = String(text);
+                // // })
+                // e.innerText = val;
+                // console.log(ref.current?.innerHTML);
                 parse3(ref.current?.innerHTML ?? "").then((text) => {
-                  console.log(text);
-                  setText(String(text));
-                  e.focus();
+                  // console.log(text);
+                  // setText(String(text));
+                  // e.focus();
                 });
+                // console.log(node);
               },
               ...props,
             }
@@ -65,8 +97,22 @@ const App: React.FC = () => {
           ),
       },
     });
-
   const parse = async (markdown: string) => await parser.process(markdown);
+
+  const logger2 = () => (tree: MdastRoot) => {
+    visit(tree, (node) => {
+      if ((node.type === "text") | (node.type === "inlineCode")) {
+        // node.id = i++;
+        // console.log(node);
+        // console.log(node.type);
+        nodes2.push(node);
+      }
+    });
+  };
+
+  const parser2 = unified().use(remarkParse).use(logger2).use(remarkStringify);
+
+  const parse2 = async (markdown: string) => await parser2.process(markdown);
 
   return (
     <Box>
@@ -77,10 +123,10 @@ const App: React.FC = () => {
           <div ref={ref}>{parsed}</div>
         </Card>
 
-        {/* <textarea
+        <textarea
           value={text}
           onChange={(e) => setText(e.currentTarget.value)}
-        /> */}
+        />
       </SimpleGrid>
     </Box>
   );
--- src/components/text/Bold.tsx
+++ src/components/text/Bold.tsx
@@ -6,6 +6,7 @@ import { Options } from "rehype-react";
 type Props = {
   children?: React.ReactNode;
   callback?(e: any): any;
+  blurCallback?(e: any): any;
   node: any;
 };
 
@@ -14,43 +15,102 @@ type Test = React.ComponentProps<
 >;
 
 const Bold: React.FC<Props> = (props) => {
-  const { children, callback, node } = props;
+  const { children, callback, node, blurCallback } = props;
+
+  // const parser = unified()
+  //   .use(remarkParse)
+  //   .use(remarkRehype)
+  //   .use(rehypeReact, {
+  //     createElement: React.createElement,
+  //     Fragment: React.Fragment,
+  //     passNode: true,
+  //     components: {
+  //       strong: (props: any) =>
+  //         React.createElement(
+  //           Bold,
+  //           {
+  //             blurCallback: (e: EventTarget & Element) => {
+  //               console.log(e.innerHTML);
+  //             },
+  //             callback: async (tokenRef) => {
+  //               // console.log(ref.current?.innerHTML);
+  //               console.log(tokenRef.current?.innerText);
+
+  //               // const val = e.innerHTML;
+  //               // console.log(val);
+  //               // e.outerHTML = val;
+  //               // e.blur();
+  //               // await sleep(0);
+  //               // // parse2(`**${val}**`).then((text) => {
+  //               // //   e.outerHTML = String(text);
+  //               // // })
+  //               // e.innerText = val;
+  //               // console.log(ref.current?.innerHTML);
+  //               // parse3(ref.current?.innerHTML ?? "").then((text) => {
+  //               // console.log(text);
+  //               // setText(String(text));
+  //               // e.focus();
+  //               // });
+  //               // console.log(node);
+  //             },
+  //             ...props,
+  //           }
+  //           // props.children
+  //         ),
+  //     },
+  //   });
+
+  // const parse = async (markdown: string) => await parser.process(markdown);
+
   // console.log(props);
   const text = useRef<React.ReactNode>(children);
+  const ref = useRef(null);
   const [isFocused, setIsFocused] = useState(false);
 
   const handleFocus = () => {
     setIsFocused(() => true);
   };
 
-  const handleBlur = () => {
+  const handleBlur: React.FocusEventHandler = (e) => {
     setIsFocused(() => false);
+    blurCallback && blurCallback(e.currentTarget);
   };
 
   const handleChange = useCallback((e: ContentEditableEvent) => {
-    text.current = e.target.value;
+    const val = e.target.value;
+    text.current = val;
     // console.log(text.current);
     // node.children[0].value = e.target.value;
     // console.log(node.children[0].value);
     // console.log(e.currentTarget);
     // callback && callback(e.currentTarget);
 
-    callback && callback(e.currentTarget);
+    // callback && callback(ref);
+    callback && callback(node);
   }, []);
 
+  const [content, setContent] = useState("");
+
+  // useEffect(() => {
+  //   parse(`**${children}**`).then((tree) => {
+  //     // console.log(tree);
+  //     setContent(String(tree));
+  //   });
+  // }, []);
+
   return (
-    <>
-      {isFocused && "**"}
+    <strong ref={ref}>
+      {/* {isFocused && "**"} */}
       <ContentEditable
-        tagName="strong"
+        tagName="span"
         onBlur={handleBlur}
         onFocus={handleFocus}
         style={{ width: "max-content" }}
         html={String(children)}
         onChange={handleChange}
       />
-      {isFocused && "**"}
-    </>
+      {/* {isFocused && "**"} */}
+    </strong>
   );
 };
 
