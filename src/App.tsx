6e95f1026d1aba95bb2452b48d8c977d169b09e3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-03 03:18:18 +0800

    Set up text processing pipeline using unified
    Also updated the app UI to show the pipeline working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { Box, Card, Code, Heading, SimpleGrid } from "@chakra-ui/react";
-import React from "react";
+import { Box, Card, Heading, SimpleGrid } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import FocusableSpan from "./components/FocusableSpan";
+import { parse } from "./utils/parser";
 
 const text =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel sunt harum quia dignissimos assumenda voluptas eaque quo distinctio eos aliquid molestiae facilis nisi enim, deleniti rerum eius aliquam est reiciendis sed? Eligendi, nemo officia rerum ab nam provident repellat deleniti numquam enim modi impedit minima omnis id ut at ad facere? Earum, suscipit rem! Id, odio enim? Quasi quia nisi suscipit consequatur, vero unde quos est doloremque. Eveniet laborum laudantium officia. Iste fuga minima accusantium provident. Enim reprehenderit, laborum amet repudiandae distinctio, saepe repellendus adipisci porro temporibus nesciunt consequuntur error quidem eaque accusantium corrupti ullam libero, ea doloremque. Quasi, atque.";
+  "Hi there, this is a test for some **bold** text, as well as `code` and _italics_ located in this paragraph.";
 
 const App: React.FC = () => {
+  const [parsed, setParsed] = useState<React.ReactNode>(<>{text}</>);
+
+  useEffect(() => {
+    parse(text).then((parsedText) => {
+      setParsed(parsedText.result);
+    });
+  }, [text]);
+
   return (
     <Box>
       <SimpleGrid spacingY={4}>
         <Heading>heapedit</Heading>
-        <Card sx={{ paddingBlock: 2, paddingInline: 3 }}>
-          <Code as="pre">
-            {text.split(". ").map((sentence) => (
-              <>
-                <FocusableSpan value={sentence} />
-                <br />
-              </>
-            ))}
-          </Code>
-        </Card>
+
+        <Card sx={{ paddingBlock: 2, paddingInline: 3 }}>{parsed}</Card>
       </SimpleGrid>
     </Box>
   );
