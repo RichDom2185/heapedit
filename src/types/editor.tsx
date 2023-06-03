e32a73e50526399094b4d035ea79390d96e32ca6
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-03 12:27:03 +0800

    Use Chakra UI components for headings and paragraphs

---
+++ src/types/editor.tsx
@@ -0,0 +1,17 @@
+import { Heading, Text } from "@chakra-ui/react";
+
+// TODO: Support more
+export type BlockComponent = "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+export const blockComponentToOwnComponentMap = Object.freeze({
+  p: Text,
+  h1: (props: any) => <Heading as="h1" {...props} />,
+  h2: (props: any) => <Heading as="h2" {...props} />,
+  h3: (props: any) => <Heading as="h3" {...props} />,
+  h4: (props: any) => <Heading as="h4" {...props} />,
+  h5: (props: any) => <Heading as="h5" {...props} />,
+  h6: (props: any) => <Heading as="h6" {...props} />,
+}) satisfies Readonly<{
+  // FIXME: Type this properly
+  [key in BlockComponent]: any;
+}>;
