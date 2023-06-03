2a8a5d78730da7bfc1edb2fb1228a27e7d31a065
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-03 12:13:38 +0800

    Setup basic editor logic and components (#1)
    * Install more dependencies
    
    * Create initial almost-there version
    
    * Attempt to improve the code again
    
    Attempted to improve by running two separate pipelines sequentially.
    
    * Conduct more testing with ref callbacks
    
    * Conduct further testing with ref callbacks
    
    * Get proof-of-concept working
    
    Finally!
    
    * Remove unused code and dependencies
    
    * Support italics and monospace text
    
    Also refactored overridden props for type-safety.
    
    * Suppress contentEditable warning
    
    * Abstract away block-component maker
    
    * Support headings 1-6 as block-level elements
    
    * Remove AST node in output components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,89 @@
 import { Box, Card, Heading, SimpleGrid } from "@chakra-ui/react";
+import { selectAll } from "hast-util-select";
+import { h } from "hastscript";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { toHast } from "mdast-util-to-hast";
 import React, { useEffect, useState } from "react";
+import rehypeReact from "rehype-react";
+import { unified } from "unified";
 import "./App.css";
-import { parse } from "./utils/parser";
+import { createBlockComponent } from "./components/editor/BlockComponent";
 
-const text =
-  "Hi there, this is a test for some **bold** text, as well as `code` and _italics_ located in this paragraph.";
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const App: React.FC = () => {
-  const [parsed, setParsed] = useState<React.ReactNode>(<>{text}</>);
+  const [text, setText] = useState(
+    "Hi there, this is a test for some **bold** text, as well as `code` and _italics_ located in this paragraph."
+  );
+  const [parsed, setParsed] = useState<React.ReactNode>("");
 
   useEffect(() => {
-    parse(text).then((parsedText) => {
-      setParsed(parsedText.result);
+    const mdast = fromMarkdown(text);
+    const hast = toHast(mdast, {});
+    selectAll("p, h1, h2, h3, h4, h5, h6", hast).forEach((node) => {
+      node.properties = {
+        ...node.properties,
+        contentEditable: true,
+        tabIndex: "-1",
+        className: "section",
+      };
     });
+    selectAll("h1", hast).forEach((node) => {
+      node.children = [h("span.token-prefix", "# "), ...node.children];
+    });
+    selectAll("h2", hast).forEach((node) => {
+      node.children = [h("span.token-prefix", "## "), ...node.children];
+    });
+    selectAll("h3", hast).forEach((node) => {
+      node.children = [h("span.token-prefix", "### "), ...node.children];
+    });
+    selectAll("h4", hast).forEach((node) => {
+      node.children = [h("span.token-prefix", "#### "), ...node.children];
+    });
+    selectAll("h5", hast).forEach((node) => {
+      node.children = [h("span.token-prefix", "##### "), ...node.children];
+    });
+    selectAll("h6", hast).forEach((node) => {
+      node.children = [h("span.token-prefix", "###### "), ...node.children];
+    });
+    selectAll("strong", hast).forEach((node) => {
+      node.children = [
+        h("span.token-prefix", "**"),
+        ...node.children,
+        h("span.token-suffix", "**"),
+      ];
+    });
+    selectAll("em", hast).forEach((node) => {
+      node.children = [
+        h("span.token-prefix", "_"),
+        ...node.children,
+        h("span.token-suffix", "_"),
+      ];
+    });
+    selectAll("code", hast).forEach((node) => {
+      node.children = [
+        h("span.token-prefix", "`"),
+        ...node.children,
+        h("span.token-suffix", "`"),
+      ];
+    });
+    setParsed(
+      unified()
+        .use(rehypeReact, {
+          createElement: React.createElement,
+          Fragment: React.Fragment,
+          components: {
+            p: createBlockComponent("p", setText),
+            h1: createBlockComponent("h1", setText),
+            h2: createBlockComponent("h2", setText),
+            h3: createBlockComponent("h3", setText),
+            h4: createBlockComponent("h4", setText),
+            h5: createBlockComponent("h5", setText),
+            h6: createBlockComponent("h6", setText),
+          },
+        })
+        .stringify(hast as any)
+    );
   }, [text]);
 
   return (
@@ -20,7 +91,17 @@ const App: React.FC = () => {
       <SimpleGrid spacingY={4}>
         <Heading>heapedit</Heading>
 
-        <Card sx={{ paddingBlock: 2, paddingInline: 3 }}>{parsed}</Card>
+        <Card sx={{ paddingBlock: 2, paddingInline: 3 }}>
+          <div>{parsed}</div>
+        </Card>
+
+        {/* FOR DEBUGGING PURPOSES */}
+        <textarea
+          value={text}
+          onChange={(e) => {
+            setText(e.currentTarget.value);
+          }}
+        />
       </SimpleGrid>
     </Box>
   );
