10cf97212149fb13bf7c11070277ae6f782c77df
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-05 00:57:03 +0800

    Create hotfix for incorrect line breaks
    For now, we simply disallow the operation that would result in weird and
    incorrect line-breaking behavior, especially when compounded multiple
    times.
    
    This only presents a minor inconvenience, and has a simple workaround,
    so it is fine to simply disallow this breaking operation for now.

--- src/components/editor/BlockComponent.tsx
+++ src/components/editor/BlockComponent.tsx
@@ -27,6 +27,17 @@ export const createBlockComponent = (
       },
       onKeyDown: (e) => {
         if (e.key === "Enter") {
+          const selection = window.getSelection();
+          const { anchorNode, anchorOffset = 0 } = selection ?? {};
+          const tokenLength = anchorNode?.textContent?.length ?? 0;
+          if (anchorOffset === 0 || anchorOffset === tokenLength) {
+            alert(
+              "I'm sorry, due to current limitations, you can't create new lines at the start/end of a paragraph 😔. Please add dummy letter, then split the paragraph, then (only after adding your actual text to both paragraphs) remove the dummy letters.\n\nThank you for understanding!"
+            );
+            e.preventDefault();
+            return;
+          }
+
           document.execCommand(
             "insertHTML",
             false,
